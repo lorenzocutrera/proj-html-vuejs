471755387ce8bc30daef1746314d30f5e49e31dd
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-10-04 13:24:32 +0200

    add sectionTwo

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,11 +1,13 @@
 <script>
 import sectionOne from './sectionOne.vue';
+import sectionTwo from './sectionTwo.vue';
 
 export default{
     name: 'appMain',
 
     components:{
-        sectionOne
+        sectionOne,
+        sectionTwo
     }
 }
 </script>
---
+++ src/components/sectionTwo.vue
@@ -0,0 +1,145 @@
+<script>
+
+export default {
+    name: 'sectionTwo'
+}
+</script>
+
+<template>
+    <div class="sectionTwo">
+        <div class="row top text-center">
+            <p>
+                Lorem ipsum dolor sit amet consectetur.
+            </p>
+            <h3>
+                Highly-skilled, independent <br> professional freelacers.
+            </h3>
+            <div>
+                <a href="#">
+                    What is a VA exactly?
+                </a>
+            </div>
+
+        </div>
+
+        <div class="container">
+            <div class="row bottom ">
+
+                <div class="col-4">
+                    <div class="card mb-3">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <h3 class="text-center">1.</h3>
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                        Voluptates iusto minus sapiente earum non blanditiis.</p>
+                                        <p>
+                                            <a href="#">Learn more</a>
+                                        </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="card mb-3">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <h3 class="text-center">2.</h3>
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                        Voluptates iusto minus sapiente earum non blanditiis.</p>
+                                        <p>
+                                            <a href="#">Learn more</a>
+                                        </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card mb-3">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <h3 class="text-center">3.</h3>
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                        Voluptates iusto minus sapiente earum non blanditiis.</p>
+                                        <p>
+                                            <a href="#">Learn more</a>
+                                        </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.sectionTwo {
+    width: 100%;
+    min-height: 600px;
+    background-color: #0d1167;
+    color: white;
+}
+
+.top {
+    padding-top: 50px;
+
+a {
+    text-decoration: none;
+    color: white;
+    background-color: #f86635;
+    padding: 10px 30px;
+    border-radius: 2px;
+    font-size: 15px;
+}
+}
+
+h3 {
+    font-size: 40px;
+    padding: 10px;
+}
+
+.col-4{
+    padding: 30px;
+}
+
+.card {
+    background-color: #000545;
+    min-height: 100px;
+    color: white;
+    border-radius: 0px;
+
+    .row{
+        padding: 10px;
+    }
+
+    a{
+        text-decoration: none;
+        color: #f86635;
+
+        &:hover{
+            text-decoration: underline;
+        }
+    }
+}
+
+.bottom {
+    margin-top: 50px;
+}
+</style>
