d345a239df69542f8a5ad454fbcba2cbd59da4fa
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-10-04 09:51:30 +0200

    add appHeader

---
+++ src/components/appHeader.vue
@@ -0,0 +1,106 @@
+<script>
+import appNavbar from './appNavbar.vue';
+
+export default {
+    name: 'appHeader',
+
+    components: {
+        appNavbar
+    }
+}
+
+</script>
+
+<template>
+    <div class="jumbotron">
+
+        <appNavbar></appNavbar>
+
+        <div class="container d-flex">
+            <div class="row d-flex">
+                <div class="sx col-6 text-center">
+                    <h3>Independent professional who works remotely to help with your business.
+                    </h3>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. nesciunt laudantium
+                        est sequi non voluptate. Voluptatem, porro?
+                    </p>
+
+                    <a href="#">
+                        What is a VA exactly?
+                    </a>
+                </div>
+                <div class="col-6">
+                    <div class="img_5">
+                        <img width="100%" src="../assets/_img/info-5.jpg" alt="">
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="badges">
+            <div class="badge_on"></div>
+            <div class="badge_on"></div>
+        </div>
+
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.jumbotron {
+    width: 100%;
+    min-height: 600px;
+    background-color: #0d1167;
+    color: white;
+}
+
+.row {
+    align-items: center;
+}
+
+
+h3 {
+    font-size: 50px;
+    padding: 50px 0px;
+}
+
+p {
+    padding-bottom: 30px;
+}
+
+a {
+    text-decoration: none;
+    background-color: white;
+    color: #0d1167;
+    padding: 10px 30px;
+    border-radius: 2px;
+    font-size: 15px;
+}
+
+.img_5 {
+    width: 100%;
+    height: 100%;
+    padding: 0px 50px;
+}
+
+.badges{
+    min-width: 50px;
+    min-height: 100px;
+    position: absolute;
+    right: 10px;
+    top: 100px;
+    
+}
+.badge_on{
+    width: 60px;
+    aspect-ratio: 1 / 1;
+    background-color: white;
+    border-radius: 2px;
+    margin-bottom: 5px;
+
+}
+</style>
---
+++ src/components/appNavbar.vue
@@ -0,0 +1,122 @@
+<script>
+
+export default {
+    name: 'appNavbar'
+}
+
+</script>
+
+<template>
+    <nav class="d-flex">
+
+        <div class="sx col-6 d-flex">
+            <div class="logo">
+                <img width="50px" src="../assets/_img/logo-2x.png" alt="">
+            </div>
+
+            <div class="pages d-flex">
+
+                <div class="page">
+                    <a href="#">Home</a>
+                </div>
+
+                <div class="page">
+                    <a href="#">About</a>
+                </div>
+
+                <div class="page">
+                    <a href="#">Service</a>
+                </div>
+
+                <div class="page">
+                    <a href="#">Journal</a>
+                </div>
+
+                <div class="page">
+                    <a href="#">Contact</a>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="dx col-6 d-flex">
+            <div class="get d-flex">
+                <div class="call">
+                   1.800.123.4567
+                </div>
+                <a href="#">
+                    Get a free consultation
+                </a>
+            </div>
+        </div>
+
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+    width: 100%;
+    height: 100px;
+    align-items: center;
+}
+
+.row {
+    justify-content: space-between;
+}
+
+.sx {
+    align-items: center;
+}
+
+.page {
+    padding: 20px;
+
+    a {
+        text-decoration: none;
+        color: white;
+        border-bottom: white;
+        padding: 10px 0px;
+    }
+
+    :hover {
+        border-bottom: solid 1px white;
+    }
+}
+
+.logo {
+    width: 50px;
+    aspect-ratio: 1 / 1;
+    margin: 0 50px;
+}
+
+.dx {
+    justify-content: flex-end;
+}
+
+.get{
+    margin: 0px 50px;
+    align-items: center;
+    color: white;
+
+    .call{
+        margin: 0px 50px;
+        padding: 10px 0px;
+
+
+        &:hover{
+            border-bottom: solid 1px white;
+        }
+    }
+
+    a{
+        text-decoration: none;
+        background-color: white;
+        padding: 10px 30px;
+        border-radius: 2px;
+        color: #0d1167;
+        font-size: 15px;
+
+    }
+}
+</style>
